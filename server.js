f03261e2a75117f6e20c03dc1586e4476569f97e
AlexDoss42 <alex.doss42@gmail.com>
2019-07-17 21:53:11 -0600

    Sets up server and controller to rock and roll

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const userCtrl = require('./userCtrl')
+
+const app = express()
+
+app.use(express.json())
+
+
+const server = app.listen(3000, () => console.log(`It's over Anakin. I have the 3000 port`))
+
+app.get('/api/user', userCtrl.getAll)
+app.get('/api/user/:userId', userCtrl.getUserById,)
+app.get('/api/admin', userCtrl.getAllAdmin)
+app.get('/api/nonadmin', userCtrl.getAllNonAdmin)
+app.get('/api/user/:userType', userCtrl.getUsersByType)
+
+app.put('/api/user/:userId', userCtrl.updateUserById)
+
+app.post('/api/user', userCtrl.createUser)
+
+app.delete('/api/user/:id', userCtrl.deleteUser)
---
+++ userCtrl.js
@@ -0,0 +1,28 @@
+const userData = require("./userData.json")
+
+module.exports = {
+  getAll: (req, res) => {
+
+  },
+  getUserById: (req, res) => {
+
+  },
+  getAllAdmin: (req, res) => {
+
+  },
+  getAllNonAdmin: (req, res) => {
+
+  },
+  getUsersByType: (req, res) => {
+
+  },
+  updateUserById: (req, res) => {
+
+  },
+  createUser: (req, res) => {
+
+  },
+  deleteUser: (req, res) => {
+
+  },
+}
